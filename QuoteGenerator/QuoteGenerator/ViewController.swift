f2985cbe744ecbd2aaa08ac0efe49509fb039b44
Yongwoo Huh <yongwoo.huh@me.com>
2020-01-11 14:30:01 +0900

    Starter for QuoteGenerator.

---
+++ QuoteGenerator/QuoteGenerator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuoteGenerator
+//
+//  Created by Yongwoo Huh on 2020/01/11.
+//  Copyright © 2020 Yongwoo Huh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
