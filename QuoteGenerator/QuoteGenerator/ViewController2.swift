0d9b1a59fd615b8c61e951492a1369e6ad40bef8
Yongwoo Huh <yongwoo.huh@me.com>
2020-01-11 15:37:26 +0900

    change .gitignore file.

---
+++ QuoteGenerator/QuoteGenerator/ViewController2.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController2.swift
+//  QuoteGenerator
+//
+//  Created by Yongwoo Huh on 2020/01/11.
+//  Copyright © 2020 Yongwoo Huh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
